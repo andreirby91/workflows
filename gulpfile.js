d6e85e8efbc28b51d4e2b8506cd7b4ac3a93c6d4
andrei.rby91@yahoo.com <andrei.rby91@yahoo.com>
2016-02-10 20:34:56 +0200

    Added gulp-htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin');
     concat = require('gulp-concat');
 
 var env,
@@ -82,7 +83,9 @@ gulp.task('connect', function(){
 
 //Use task reload for html files
 gulp.task('html', function(){
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+  .pipe(gulpif(env === 'prod', htmlmin({collapseWhitespace: true})))
+  .pipe(gulpif(env === 'prod', gulp.dest(outputDir)))
 	.pipe(connect.reload())
 });
 
@@ -97,7 +100,7 @@ gulp.task('watch', function(){
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
